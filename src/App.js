0fd20afce7134f44436af1ea6fd89a71746fc344
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-04-27 13:58:36 +0000

    08-05 Now the Table should be Visible

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./layout/Navbar";
-import Home from './pages/home';
+import Home from './pages/Home';
 
 function App() {
   return (
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,25 +4,25 @@ export default function Home() {
   return (
     <div classsName='container'>
         <div className="py-4">
-        <table class="table">
-        <thead>
-            <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            </tr>
-        </tbody>
-        </table>
-              </div>  
+            <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>  
     </div>
   )
 }
