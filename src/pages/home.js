d252a56f2e69ba467aa3a15ab425c25acb362203
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-04-27 16:43:23 +0000

    09-02 connect it with backend to loadUsers

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,12 +3,19 @@ import axios from "axios";
 
 export default function Home() {
 
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
 
   useEffect(() => {
-    console.log("Checking the Console Loading...")
-  });
+    loadUsers();
+  }, []);
+
+
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:8080/users");
+    console.log(result);
+    setUsers(result.data);
+  };
 
   // Verify the Console
 
