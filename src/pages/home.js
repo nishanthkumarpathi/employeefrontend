0f659a3e3c0d6d7cc4138e86b18594f59c8c528b
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-04-27 13:54:35 +0000

    08-03 Created the Table as a Dummy Value

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,9 +3,26 @@ import React from 'react'
 export default function Home() {
   return (
     <div classsName='container'>
-
-
-        
+        <div className="py-4">
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+        </tbody>
+        </table>
+              </div>  
     </div>
   )
 }
