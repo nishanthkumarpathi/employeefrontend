e770a16cb1b560444da41dc3ce0d53ed43a4f766
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-04-27 13:55:15 +0000

    08-04 Import Home to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./layout/Navbar";
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
 
       <Navbar />
+      <Home />
 
     </div>
   );
