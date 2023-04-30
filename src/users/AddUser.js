9cd85e4b9e903ad8b3e14c552027fb34167a82c9
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-04-30 13:12:56 +0300

    11-01 added a html template box

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function AddUser() {
   return (
-    <div>AddUser Form</div>
+    <div className='Container'>
+        <div className='row'>
+            <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow"></div>
+        </div>
+                
+    </div>
   )
 }
