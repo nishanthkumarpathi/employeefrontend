92462c5a77cea68b0317278a564892eaa4872c6b
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-04-30 13:09:13 +0300

    10-04 integrate react route with html frontend

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
@@ -22,8 +23,7 @@ export default function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
 
-        <button className="btn btn-outline-light" >Add Employee</button>
-
+        <Link className="btn btn-outline-light" to="/adduser" >Add User</Link>
 
         </div>
       </nav>
