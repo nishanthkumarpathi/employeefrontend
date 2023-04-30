6223cd5e6b7971db530172269c0d2e58f00225d1
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-04-30 13:29:47 +0300

    12-01 Store the Information in the State

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,24 +1,40 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function AddUser() {
+
+    // useState Hook for User
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+    });
+
+    // Object Destructuring
+    const { name, username, email } = user;
+
+
   return (
     <div className='Container'>
         <div className='row'>
+            {/* Bootstrap Grid System */}
             <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
                 <h2 className="text-center m-4">Register User</h2>
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="name" placeholder="Enter your Name" />
+                    <input type="text" className="form-control" name="name" placeholder="Enter your Name" value={name}/>
                 </div>
 
                 <div className="mb-3">
                     <label htmlFor="username" className="form-label">UserName</label>
-                    <input type="text" className="form-control" id="name" placeholder="Enter your User Name" />
+                    <input type="text" className="form-control" name="username" placeholder="Enter your User Name" value={username
+                    }/>
                 </div>
 
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email</label>
-                    <input type="text" className="form-control" id="name" placeholder="Enter your Email" />
+                    <input type="text" className="form-control" name="email" placeholder="Enter your Email" value={email
+                    }/>
                 </div>
 
                 <button type="submit" className="btn btn-outline-primary">Submit</button>
