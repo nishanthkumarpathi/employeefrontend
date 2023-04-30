81fc247d6404ff94018262e678c4d12f6ea211b3
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-04-30 13:15:20 +0300

    11-03 Name Label Added

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -6,6 +6,10 @@ export default function AddUser() {
         <div className='row'>
             <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
                 <h2 className="text-center m-4">Register User</h2>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name</label>
+
+                </div>
             </div>
         </div>
                 
